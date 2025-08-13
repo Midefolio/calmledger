356c7273254db2e1a1c2ee3da02519fb2f0092f2
Ayomide Babatunde Olosho <87127660+Midefolio@users.noreply.github.com>
2025-08-13 15:19:47 +0100

    prol

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import nodePolyfills from 'vite-plugin-node-polyfills';
-import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -30,3 +31,4 @@ export default defineConfig({
     }
   }
 });
+
