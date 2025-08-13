e8a7b5181300c4c9ef57571229f8bb21b770afbf
Ayomide Babatunde Olosho <87127660+Midefolio@users.noreply.github.com>
2025-08-13 15:17:51 +0100

    sdjk

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import nodePolyfills from 'vite-plugin-node-polyfills';
+import tailwindcss from 'tailwindcss';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), nodePolyfills({
-    include: ['stream'],
-    globals: { Buffer: true, process: true }
-  })],
-    optimizeDeps: {
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills({
+      include: ['stream'],
+      globals: { Buffer: true, process: true }
+    })
+  ],
+  optimizeDeps: {
     include: ['@solana/wallet-adapter-wallets'],
-  },
-  server: {
-    port: 3000
   },
   build: {
     rollupOptions: {
+      external: ['@solana/wallet-adapter-wallets'], // 👈 prevents Rollup from trying to bundle it
       plugins: [
         nodePolyfills({
           include: ['stream'],
           globals: { Buffer: true, process: true }
         })
       ]
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
     }
   }
-})
+});
